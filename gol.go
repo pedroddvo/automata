7beaf5d7052f558c464820724d7f4f9b9947f64b
pedroddvo <pedroddvo@gmail.com>
2022-03-13 00:52:55 +0000

    Game of life

---
+++ gol.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+const (
+	GolHeight = 15
+	GolWidth  = 10
+)
+
+type Cell bool
+
+type Board struct {
+	cells [GolHeight * GolWidth]Cell
+}
+
+func (b *Board) Cell(x, y int) *Cell {
+	return &b.cells[(y*GolWidth)+x]
+}
+
+// includes itself
+func (b *Board) Neighbours(x, y int) (n int) {
+	// constrain size of square for no index error
+	for j := -1; j <= 1; j++ {
+		for i := -1; i <= 1; i++ {
+			wrappedX := (x + i + GolWidth) % GolWidth
+			wrappedY := (y + j + GolHeight) % GolHeight
+			if (j != 0 || i != 0) && *b.Cell(wrappedX, wrappedY) {
+				n++
+			}
+		}
+	}
+
+	return
+}
+
+func (b *Board) Step() {
+	// copy board
+	copy := *b
+	for j := 0; j < GolHeight; j++ {
+		for i := 0; i < GolWidth; i++ {
+			n, cell := b.Neighbours(i, j), *b.Cell(i, j)
+			stepCell := (!cell && n == 3) || (cell && (n == 3 || n == 2))
+			*copy.Cell(i, j) = stepCell
+		}
+	}
+
+	*b = copy
+}
+
+func (b *Board) Draw() {
+	for j := 0; j < GolHeight; j++ {
+		for i := 0; i < GolWidth; i++ {
+			if *b.Cell(i, j) {
+				fmt.Print("#")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Print("\n")
+	}
+}
+
+func NewBoard() Board {
+	return Board{}
+}
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+)
+
+const platform = runtime.GOOS
+
+// multi platform console clear
+func Clear() {
+	var cmd *exec.Cmd
+	if platform == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else if platform == "linux" {
+		cmd = exec.Command("clear")
+	} else {
+		return
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
 
 func main() {
-	fmt.Println("Hello")
+	board := NewBoard()
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		rand.Seed(rand.Int63())
+		for i := 0; i < 50; i++ {
+			*board.Cell(rand.Intn(GolWidth), rand.Intn(GolHeight)) = true
+		}
+
+		for {
+			Clear()
+			board.Step()
+			board.Draw()
+
+			r, _, err := reader.ReadRune()
+			if err != nil {
+				panic(err)
+			}
+			if r == 'n' {
+				break
+			}
+		}
+	}
 }
